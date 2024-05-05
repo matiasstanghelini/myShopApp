d230fe39e2fe8dc7a8a3a54370748b3bf9129a66
matias <stanghelinimatias@gmail.com>
2024-05-05 15:31:40 -0300

    Se corrige el texto de la app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My First App!!!!</Text>
+      <Text>My First App!</Text>
       <StatusBar style="auto" />
     </View>
   );
